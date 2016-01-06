900093ebc256513ecb4b73732d8fbaf472ddbfbc
Shubham Agrawal <shubhamagrawal@Shubhams-MacBook-Pro.local>
2016-01-07 02:08:11 +0530

    added more changes to config

--- filebeatconfig.sh
+++ filebeatconfig.sh
@@ -1,9 +1,11 @@
 echo "Starting file beat installation" 
-curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
 sudo apt-get update && sudo apt-get install filebeat
 echo "Installation complete, Adding config files."
 sudo wget -O /etc/filebeat/filebeat.yml "https://raw.githubusercontent.com/shubhamagrawal100/filebeatconfig/master/filebeat.template.json"
 sudo wget -O /etc/filebeat/filebeat.yml "https://raw.githubusercontent.com/shubhamagrawal100/filebeatconfig/master/filebeat.yml"
 echo "Files added. Will now start filebeat. If you want to stop use \"sudo service filebeat stop\" "
-
+sudo update-rc.d filebeat defaults
+sudo service filebeat start 
+echo "File beat started, please check /var/log/mybeat for logs to see if everything is working"
