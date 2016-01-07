2301880458d8d3a2b05a3f9325adc75dc39e254b
Shubham Agrawal <shubhamagrawal@Shubhams-MacBook-Pro.local>
2016-01-07 19:41:23 +0530

    added echo

--- newbeat.sh
+++ newbeat.sh
@@ -1,5 +1,6 @@
 sudo wget -O /etc/filebeat/filebeat.yml "https://raw.githubusercontent.com/shubhamagrawal100/filebeatconfig/master/filebeat.yml"
 echo "Files added. Will now start filebeat. If you want to stop use \"sudo service filebeat stop\" "
+echo "Restarting...."
 sudo service filebeat restart 
 echo "File beat restarted, please check /var/log/mybeat for logs to see if everything is working"
 
