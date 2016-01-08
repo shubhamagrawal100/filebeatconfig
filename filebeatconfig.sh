b713d4b1b67e72ce0fbbb23f25be118eb2e5f727
Shubham Agrawal <shubhamagrawal@Shubhams-MacBook-Pro.local>
2016-01-08 15:57:53 +0530

    removed json

--- filebeatconfig.sh
+++ filebeatconfig.sh
@@ -3,7 +3,6 @@ sudo curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-ke
 echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
 sudo apt-get update && sudo apt-get install filebeat
 echo "Installation complete, Adding config files."
-sudo wget -O /etc/filebeat/filebeat.yml "https://raw.githubusercontent.com/shubhamagrawal100/filebeatconfig/master/filebeat.template.json"
 sudo wget -O /etc/filebeat/filebeat.yml "https://raw.githubusercontent.com/shubhamagrawal100/filebeatconfig/master/filebeat.yml"
 echo "Files added. Will now start filebeat. If you want to stop use \"sudo service filebeat stop\" "
 sudo update-rc.d filebeat defaults
